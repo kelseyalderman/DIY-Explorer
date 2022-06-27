c3909470b87a010ddd9e47d75b69b28c6c255596
Kelsey Alderman <kelseyalderman@gmail.com>
2022-06-27 08:42:27 -0400

    set up User model

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/DIY-sharing", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
