09d4e653802e38878ddcd922258ad4b33be81e02
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-28 12:17:10 -0700

    write all queries for frontend

---
+++ client/src/utils/queries.js
@@ -0,0 +1,87 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql `
+  {
+    me {
+      _id
+      username
+      email
+      projects {
+        _id
+        projectText
+        createdAt
+        commentCount
+        comments {
+          _id
+          createdAt
+          reactionBody
+          username
+        }
+      }
+      savedProjects {
+        _id
+        projectText
+        createdAt
+        username
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql `
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      projects {
+        _id
+        projectText
+        createdAt
+        commentCount
+      }
+      savedProjects {
+        _id
+        projectText
+        createdAt
+        username
+      }
+    }
+  }
+`;
+
+export const QUERY_PROJECTS = gql `
+  query projects($username: String) {
+    projects(username: $username) {
+        _id
+        projectText
+        createdAt
+        username
+        commentCount
+        comments {
+          _id
+          reactionBody
+          createdAt
+          username
+        }
+    }
+  }
+`;
+
+export const QUERY_PROJECT = gql `
+  query project($id: ID!) {
+    project(_id: $id) {
+        _id
+        projectText
+        createdAt
+        username
+        commentCount
+        comments {
+          _id
+          reactionBody
+          createdAt
+          username
+        }
+    }
+  }
+`;
