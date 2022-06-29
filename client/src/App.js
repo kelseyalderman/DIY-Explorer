68cb0486252ed349efa943e0619c3d49dec94383
Kelsey Alderman <kelseyalderman@gmail.com>
2022-06-29 09:41:35 -0400

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const httpLink = createHttpLink({
   uri: "/graphql",
@@ -43,6 +44,7 @@ function App() {
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
             </Routes>
           </div>
           <Footer />
