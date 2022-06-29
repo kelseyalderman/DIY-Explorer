0b4d58de666c5ceb617ee0c957aa5c91323e24ce
Kelsey Alderman <kelseyalderman@gmail.com>
2022-06-29 08:50:14 -0400

    saved projects component

---
+++ client/src/components/SavedProjects/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SavedProjectList = ({ username, savedProjects }) => {
+  if (!savedProjects || !savedProjects.length) {
+    return (
+      <p className="bg-dark text-light p-3">
+        {username}, No projects saved yet!
+      </p>
+    );
+  }
+
+  return (
+    <div>
+      <h5>{username}'s Saved Projects</h5>
+      {savedProjects.map((savedProject) => (
+        <button className="btn w-100 display-block mb-2" key={savedProject._id}>
+          <Link to={`/project/${savedProject._id}`}>{savedProject.title}</Link>
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default SavedProjectList;
